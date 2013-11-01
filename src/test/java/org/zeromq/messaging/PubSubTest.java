9627f8af0ad27848dc0c8a42d07b5908a82e8404
v-avysochin <artem.vysochyn@gmail.com>
2013-11-01 07:32:45 -0700

    - fixed pub/sub tests. - updated logback.

--- src/test/java/org/zeromq/messaging/PubSubTest.java
+++ src/test/java/org/zeromq/messaging/PubSubTest.java
@@ -24,8 +24,6 @@ import com.google.common.base.Stopwatch;
 import org.junit.Test;
 import org.zeromq.TestRecorder;
 
-import java.util.concurrent.TimeUnit;
-
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.zeromq.messaging.ZmqAbstractTest.Fixture.HELLO;
 
@@ -33,26 +31,26 @@ public class PubSubTest extends ZmqAbstractTest {
 
   static class Fixture {
 
-    private ZmqContext zmqContext;
+    final ZmqContext zmqContext;
 
     Fixture(ZmqContext zmqContext) {
       this.zmqContext = zmqContext;
-    }
-
-    ZmqChannel newSubscriber() {
-      return ZmqChannelFactory.builder()
-                              .withZmqContext(zmqContext)
-                              .ofSUBType()
-                              .withConnectAddress("tcp://localhost:1234")
-                              .build()
-                              .newChannel();
     }
 
     ZmqChannel newPublisher() {
       return ZmqChannelFactory.builder()
                               .withZmqContext(zmqContext)
                               .ofPUBType()
-                              .withBindAddress("tcp://*:1234")
+                              .withBindAddress("inproc://p")
+                              .build()
+                              .newChannel();
+    }
+
+    ZmqChannel newSubscriber() {
+      return ZmqChannelFactory.builder()
+                              .withZmqContext(zmqContext)
+                              .ofSUBType()
+                              .withConnectAddress("inproc://p")
                               .build()
                               .newChannel();
     }
@@ -61,106 +59,117 @@ public class PubSubTest extends ZmqAbstractTest {
   @Test
   public void t0() throws InterruptedException {
     Fixture f = new Fixture(zmqContext());
-    ZmqChannel subscriber = f.newSubscriber();
     ZmqChannel publisher = f.newPublisher();
+    ZmqChannel subscriber = f.newSubscriber();
 
-    byte[] topicA = "A".getBytes();
-    byte[] topicB = "B".getBytes();
-    byte[] topicC = "C".getBytes();
+    try {
+      byte[] topicA = "A".getBytes();
+      byte[] topicB = "B".getBytes();
+      byte[] topicC = "C".getBytes();
 
-    subscriber.subscribe(topicA);
-    subscriber.subscribe(topicB);
-    subscriber.subscribe(topicC);
+      subscriber.subscribe(topicA);
+      subscriber.subscribe(topicB);
+      subscriber.subscribe(topicC);
 
-    TimeUnit.SECONDS.sleep(1);
+      // send message with topic A.
+      publisher.send(ZmqMessage.builder(HELLO()).withTopic(topicA).build());
+      // send message with topic B.
+      publisher.send(ZmqMessage.builder(HELLO()).withTopic(topicB).build());
+      // send message with topic C.
+      publisher.send(ZmqMessage.builder(HELLO()).withTopic(topicC).build());
 
-    // send message with topic A.
-    publisher.send(ZmqMessage.builder(HELLO()).withTopic(topicA).build());
-    // send message with topic B.
-    publisher.send(ZmqMessage.builder(HELLO()).withTopic(topicB).build());
-    // send message with topic C.
-    publisher.send(ZmqMessage.builder(HELLO()).withTopic(topicC).build());
+      // send other messages.
+      for (int i = 0; i < 3; i++) {
+        publisher.send(HELLO());
+      }
 
-    // send other messages.
-    for (int i = 0; i < 3; i++) {
-      publisher.send(HELLO());
+      // receive three messages.
+      assert subscriber.recv() != null;
+      assert subscriber.recv() != null;
+      assert subscriber.recv() != null;
+      // assert that other messages can't be received because of topic subscription.
+      assert subscriber.recv() == null;
     }
-
-    // receive three messages.
-    assert subscriber.recv() != null;
-    assert subscriber.recv() != null;
-    assert subscriber.recv() != null;
-    // assert that other messages can't be received because of topic subscription.
-    assert subscriber.recv() == null;
+    finally {
+      subscriber.destroy();
+      publisher.destroy();
+    }
   }
 
   @Test
   public void t1() throws InterruptedException {
     Fixture f = new Fixture(zmqContext());
-    ZmqChannel subscriber = f.newSubscriber();
     ZmqChannel publisher = f.newPublisher();
+    ZmqChannel subscriber = f.newSubscriber();
 
-    byte[] topic = "topic".getBytes();
+    try {
+      byte[] topic = "topic".getBytes();
+      // subscribe just once.
+      subscriber.subscribe(topic);
 
-    TimeUnit.SECONDS.sleep(1);
+      // send messages with topic.
+      for (int i = 0; i < 4; i++) {
+        publisher.send(ZmqMessage.builder(HELLO()).withTopic(topic).build());
+      }
 
-    // subscribe just once.
-    subscriber.subscribe(topic);
-
-    // send messages with topic.
-    for (int i = 0; i < 4; i++) {
-      publisher.send(ZmqMessage.builder(HELLO()).withTopic(topic).build());
+      // receive only three messages.
+      assert subscriber.recv() != null;
+      assert subscriber.recv() != null;
+      assert subscriber.recv() != null;
+      // unsubscribe just once.
+      subscriber.unsubscribe(topic);
+      // assert that messages can't be received.
+      assert subscriber.recv() == null;
     }
-
-    // receive only three messages.
-    assert subscriber.recv() != null;
-    assert subscriber.recv() != null;
-    assert subscriber.recv() != null;
-    // unsubscribe just once.
-    subscriber.unsubscribe(topic);
-    // assert that messages can't be received.
-    assert subscriber.recv() == null;
+    finally {
+      subscriber.destroy();
+      publisher.destroy();
+    }
   }
 
   @Test
   public void t2() throws InterruptedException {
     Fixture f = new Fixture(zmqContext());
-    ZmqChannel subscriber = f.newSubscriber();
     ZmqChannel publisher = f.newPublisher();
+    ZmqChannel subscriber = f.newSubscriber();
 
-    TimeUnit.SECONDS.sleep(1);
+    try {
+      // subscribe many times.
+      int numOfSubscr = 17;
+      // topic.
+      byte[] topic = "topic".getBytes();
+      for (int i = 0; i < numOfSubscr; i++) {
+        subscriber.subscribe(topic);
+      }
 
-    // subscribe many times.
-    int numOfSubscr = 17;
-    // topic.
-    byte[] topic = "topic".getBytes();
-    for (int i = 0; i < numOfSubscr; i++) {
-      subscriber.subscribe(topic);
-    }
+      // send messages with topic.
+      for (int i = 0; i < 6; i++) {
+        publisher.send(ZmqMessage.builder(HELLO()).withTopic(topic).build());
+      }
 
-    // send messages with topic.
-    for (int i = 0; i < 6; i++) {
-      publisher.send(ZmqMessage.builder(HELLO()).withTopic(topic).build());
-    }
-
-    // receive only three messages.
-    assert subscriber.recv() != null;
-    assert subscriber.recv() != null;
-    assert subscriber.recv() != null;
-    // unsubscribe just once.
-    subscriber.unsubscribe(topic);
-    // check that you can get messages because subscriptions are there yet.
-    assert subscriber.recv() != null;
-    // unsubscribe again.
-    subscriber.unsubscribe(topic);
-    // check again.
-    assert subscriber.recv() != null;
-    // unsubscribe fully.
-    for (int i = 0; i < numOfSubscr; i++) {
+      // receive only three messages.
+      assert subscriber.recv() != null;
+      assert subscriber.recv() != null;
+      assert subscriber.recv() != null;
+      // unsubscribe just once.
       subscriber.unsubscribe(topic);
+      // check that you can get messages because subscriptions are there yet.
+      assert subscriber.recv() != null;
+      // unsubscribe again.
+      subscriber.unsubscribe(topic);
+      // check again.
+      assert subscriber.recv() != null;
+      // unsubscribe fully.
+      for (int i = 0; i < numOfSubscr; i++) {
+        subscriber.unsubscribe(topic);
+      }
+      // assert that messages can't be received.
+      assert subscriber.recv() == null;
     }
-    // assert that messages can't be received.
-    assert subscriber.recv() == null;
+    finally {
+      subscriber.destroy();
+      publisher.destroy();
+    }
   }
 
   @Test
@@ -171,17 +180,22 @@ public class PubSubTest extends ZmqAbstractTest {
     Fixture f = new Fixture(zmqContext());
     ZmqChannel publisher = f.newPublisher();
 
-    int ITER = 500;
-    int MESSAGE_NUM = 1000;
-    ZmqMessage message = ZmqMessage.builder(HELLO())
-                                   .withTopic("xyz".getBytes())
-                                   .build();
-    Stopwatch timer = new Stopwatch().start();
-    for (int j = 0; j < ITER; j++) {
-      for (int i = 0; i < MESSAGE_NUM; i++) {
-        assert publisher.send(message);
+    try {
+      int ITER = 500;
+      int MESSAGE_NUM = 1000;
+      ZmqMessage message = ZmqMessage.builder(HELLO())
+                                     .withTopic("xyz".getBytes())
+                                     .build();
+      Stopwatch timer = new Stopwatch().start();
+      for (int j = 0; j < ITER; j++) {
+        for (int i = 0; i < MESSAGE_NUM; i++) {
+          assert publisher.send(message);
+        }
       }
+      r.logQoS((ITER * MESSAGE_NUM) / timer.stop().elapsedTime(MILLISECONDS), "messages/ms.");
     }
-    r.logQoS((ITER * MESSAGE_NUM) / timer.stop().elapsedTime(MILLISECONDS), "messages/ms.");
+    finally {
+      publisher.destroy();
+    }
   }
 }
